c2699fff78824ed40660523b341fb884ae06158d
rajantruestate <160835586+rajantruestate@users.noreply.github.com>
2024-04-13 19:26:21 +0530

    project-init

---
+++ upload.js
@@ -0,0 +1,46 @@
+const { chromium } = require("playwright");
+const path = require("path");
+const fs = require("fs");
+
+const textResponse = "This is a dog.";
+
+(async () => {
+  const browser = await chromium.launchPersistentContext("./data", {
+    headless: false,
+  });
+
+  // upload
+  const newpage = await browser.newPage();
+  await newpage.goto("https://instagram.com/");
+
+  const createButton = await newpage.waitForSelector(
+    'svg[aria-label="New post"]'
+  );
+  await createButton.click();
+
+  await newpage.setInputFiles('input[type="file"]', "./dog.jpg");
+
+  const nextButton = await newpage.waitForSelector(
+    '.x9f619.xjbqb8w.x78zum5.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.xyamay9.x1pi30zi.x1l90r2v.x1swvt13.x1n2onr6.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.xdt5ytf.xqjyukv.x1qjc9v5.x1oa3qoh.x1nhvcw1 div[role="button"]'
+  );
+  await nextButton.click();
+  console.log(0);
+  const nextButton2 = await newpage.waitForSelector(
+    "div._ac7b._ac7d div.x1i10hfl.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.xdl72j9.x2lah0s.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x2lwn1j.xeuugli.x1hl2dhg.xggy1nq.x1ja2u2z.x1t137rt.x1q0g3np.x1lku1pv.x1a2a7pz.x6s0dn4.xjyslct.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x9f619.x1ypdohk.x1f6kntn.xwhw2v2.xl56j7k.x17ydfre.x2b8uid.xlyipyv.x87ps6o.x14atkfc.xcdnw81.x1i0vuye.xjbqb8w.xm3z3ea.x1x8b98j.x131883w.x16mih1h.x972fbf.xcfux6l.x1qhh985.xm0m39n.xt0psk2.xt7dq6l.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1n2onr6.x1n5bzlp.x173jzuc.x1yc6y37",
+    { visible: true }
+  );
+
+  await nextButton2.click();
+
+  const captionElement = await newpage.waitForSelector(
+    'div[aria-label="Write a caption..."]'
+  );
+  
+  await captionElement.type(textResponse);
+
+  const shareButton = await newpage.waitForSelector(
+    "div._ac7b._ac7d div.x1i10hfl.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.xdl72j9.x2lah0s.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x2lwn1j.xeuugli.x1hl2dhg.xggy1nq.x1ja2u2z.x1t137rt.x1q0g3np.x1lku1pv.x1a2a7pz.x6s0dn4.xjyslct.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x9f619.x1ypdohk.x1f6kntn.xwhw2v2.xl56j7k.x17ydfre.x2b8uid.xlyipyv.x87ps6o.x14atkfc.xcdnw81.x1i0vuye.xjbqb8w.xm3z3ea.x1x8b98j.x131883w.x16mih1h.x972fbf.xcfux6l.x1qhh985.xm0m39n.xt0psk2.xt7dq6l.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1n2onr6.x1n5bzlp.x173jzuc.x1yc6y37",
+    { visible: true }
+  );
+  await shareButton.click();
+})();
